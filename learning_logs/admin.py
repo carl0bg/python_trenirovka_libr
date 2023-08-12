894759e8fa19292cd4c02e259eb6b3649eb429fa
Карлышев Дмитрий <119626329+carl0bg@users.noreply.github.com>
2023-08-13 01:13:48 +0300

    Delete learning_logs directory

--- learning_logs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Entry
-
-admin.site.register(Topic) #Сообщает Django что управление моделью должно осуществляться через административный сайт
-admin.site.register(Entry)
-
-# Register your models here.
