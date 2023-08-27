aed023b89d8d7d894aabe002eb5f9f0b49da17a8
Карлышев Дмитрий <119626329+carl0bg@users.noreply.github.com>
2023-08-27 17:33:42 +0300

    Add files via upload

--- trenirovka.py
+++ trenirovka.py
@@ -1,10 +1,28 @@
-def tpl_sort(s):
-    tupl = tuple(s)
-    for per in tupl:
-        if type(per) is not int:
-            return tupl
-    print('hel')
-    return sorted(tupl)
+import operator
+#import functools
+from functools import lru_cache
+import requests #библиотека для бысрой работы с запросами http
+# numbers = [1, 2, 3, 4, 5, 6]
+# summed = functools.reduce(operator.add, numbers)
+# product= functools.reduce(operator.mul, numbers)
+# print(summed)
+# print(product)
 
+# @lru_cache(maxsize = 32)
+# def get_with_cache(url):
+#     try:
+#         r = requests.get(url)
+#     except:
+#         return "Not found"
+    
+# for url in ["https://google.com/",
+#             "https://martinheinz.dev/",
+#             "https://reddit.com/"]:
+#     get_with_cache(url)
 
-print(tpl_sort([1000, 45.3, 40, 30]))
+#print(get_with_cache.cache_info())
+
+res = requests.get("https://skillbox.ru") #получаем информацию о страницы
+content = res.content #поулучаем контент страницы
+content = res.text #удобночитаем информация страницы
+print(content) 
