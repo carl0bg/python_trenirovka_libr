0b16c8093dfb814c87e3b03536c1339bf9f1c5a5
Карлышев Дмитрий <119626329+carl0bg@users.noreply.github.com>
2023-09-01 15:11:48 +0300

    Add files via upload

--- codewars2.py
+++ codewars2.py
@@ -1,30 +1,33 @@
 '''
-You are going to be given an array of integers.
-Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
-If there is no index that would make this happen, return -1
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 '''
 
-def find_even_index(arr):
-    perem = sum(arr)/2
-    if (len(arr) == 0):
-        return 0
-    perem2=0
-    p = 0
-    while p!=len(arr):
-        if perem2+(arr[p])/2 == perem:
-            return p
-        perem2 += arr[p]
-        p+=1
-    return (-1)
+def likes(names):
+   if (len(names)==0):
+      return 'no one likes this'
+   elif (len(names)==1):
+      return '{0} likes this'.format(*names)
+   elif (len(names) == 2):
+      return '{0} and {1} like this'.format(*names)
+   elif (len(names == 3)):
+      return '{0}, {1} and {2} like this'.format(*names)
+   else:
+      return f'{names[0]}, {names[1]}, and {len(names)-2} others like this'
 
 '''
-def find_even_index(arr):
-    for i in range(len(arr)):
-        if sum(arr[:i]) == sum(arr[i+1:]):
-            return i
-    return -1
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
+
+print(likes(['Alex', 'Jacob', 'Mark', 'Max']))
 '''
-print(find_even_index([1,2,3,4,3,2,1]))
-
-
-    
