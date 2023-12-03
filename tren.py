cb60e9448bee7e4cbcdd665d0861c87a094296df
Карлышев Дмитрий <119626329+carl0bg@users.noreply.github.com>
2023-12-03 15:36:42 +0300

    @classmethod

--- tren.py
+++ tren.py
@@ -1,8 +1,24 @@
-import pprint
-
-lst = ["http.11111", "htpp.22222", "htpp.33333"]
-
-d = dict(zip(lst, ["1", "2", "3"]))
 
 
-pprint.pprint(d)  # удобно выводит словарь
+class A:
+   x = 5
+   y= 2
+
+   def __init__(self, a:float):
+      self.a = a
+
+   # def add(self, a:int, b:int):
+   #    return a+b
+ 
+   @classmethod #помогает использовать какую то функцию относительно нашего класса
+   def sub(cls):
+      return cls(13) #выходит экземпляр класса
+      #return cls.x - cls.y
+
+
+print(A.sub().__dict__)
+
+
+# instance = A()
+# A.add =classmethod(A.add)
+
